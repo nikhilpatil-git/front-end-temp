5b51fceaf1cb21073ea0ad7fb4d88be71a77cce4
nikhilpatil-git <patilnikhil9@gmail.com>
2019-08-27 11:24:39 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-//   entry: './src/index',
-//   output: {
-//     path: path.join(__dirname, '/dist'),
-//     filename: 'bundle.js'
-//   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
@@ -31,44 +26,3 @@ module.exports = {
     })
   ]
 };
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-//
-// module.exports = {
-//   output: {
-//     path: path.join(__dirname, '/dist'),
-//     filename: 'bundle.js'
-//   },
-//   resolve: {
-//     extensions: ['.ts', '.tsx', '.js']
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(ts|js)x?$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader'
-//         },
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [MiniCssExtractPlugin.loader, "css-loader"]
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: "./src/index.html"
-//     }),
-//     new MiniCssExtractPlugin({
-//       filename: "[name].css",
-//       chunkFilename: "[id].css"
-//     })
-//   ],
-//   optimization: {
-//     minimize: true
-//   }
-// };
